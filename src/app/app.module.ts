cf53fad4d900699541af0ff565fca6a02a37d80e
Klipach <klipach@mail.ru>
2020-09-25 15:18:04 +0300

    First Commit Empty Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainschoolComponent } from './mainschool/mainschool.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+// определение маршрутов
+const appRoutes: Routes = [
+  {path: '', component: MainschoolComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainschoolComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
