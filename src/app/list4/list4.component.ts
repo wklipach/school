a0f6b579f67806db58f370d3dcf5f2d984294fe8
yalayas <yal@yal.spb.ru>
2020-10-06 14:27:05 +0300

    Лист 2

---
+++ src/app/list4/list4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list4',
+  templateUrl: './list4.component.html',
+  styleUrls: ['./list4.component.css']
+})
+export class List4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
