51deaf8a15989c6b9b3c52188a9c1858c302e9d1
Klipach <klipach@mail.ru>
2021-03-15 15:08:45 +0300

    Какаято фигня

--- src/app/list5/list5.component.ts
+++ src/app/list5/list5.component.ts
@@ -14,6 +14,7 @@ import {Guide7Service} from '../components/guide7/guide7.service';
 })
 export class List5Component implements OnInit {
 
+  thisThemeId = -1;
   thisTheme = '---';
   edititing_id = '-1';
   typeEdit = 'новый документ';
@@ -124,6 +125,7 @@ export class List5Component implements OnInit {
         const document = value[0];
         if (document.objSummaryLesson.documentTypeLesson) {
           this.thisTheme = document.objSummaryLesson.documentTypeLesson.title;
+          this.thisThemeId = document.objSummaryLesson.documentTypeLesson.id;
         }
       }
     });
